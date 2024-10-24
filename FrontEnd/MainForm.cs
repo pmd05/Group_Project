a09aa92781a00a3e15f35809fcb87079eebf10db
Ei-Erik <VanHuytqn0802@gmail.com>
2024-10-24 15:19:57 +0700

    Update MainForm.cs

--- FrontEnd/MainForm.cs
+++ FrontEnd/MainForm.cs
@@ -12,31 +12,6 @@ namespace Chatgpt
 {
     public partial class MainForm : Form
     {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
-
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-
-        }
-        private void btnBookAppointment_Click(object sender, EventArgs e)
-        {
-            AppointmentForm appointmentForm = new AppointmentForm();
-            appointmentForm.Show();
-        }
-
-        private void btnManageAppointments_Click(object sender, EventArgs e)
-        {
-            ManageAppointmentsForm manageAppointmentsForm = new ManageAppointmentsForm();
-            manageAppointmentsForm.Show();
-        }
-
-        private void btnViewRewards_Click(object sender, EventArgs e)
-        {
-            RewardsForm rewardsForm = new RewardsForm();
-            rewardsForm.Show();
-        }
+       
     }
 }
