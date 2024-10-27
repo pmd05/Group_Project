308b30659e6c1d06765b82b780dd3bc5570018cf
vovankiet-046205001049 <kietvv1049@ut.edu.vn>
2024-10-27 17:48:24 +0700

    Update AppointmentBookingandManagement.cs

--- BackEnd/AppointmentBookingandManagement.cs
+++ BackEnd/AppointmentBookingandManagement.cs
@@ -1,18 +1,6 @@
 // Khách hàng: Phát triển giao diện và logic cho phép khách hàng duyệt dịch vụ, chọn stylist và đặt lịch hẹn.
 // Nhân viên salon: Xây dựng hệ thống quản lý và theo dõi yêu cầu lịch hẹn, bao gồm việc chấp nhận và từ chối các yêu cầu.
-public class Service
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    
-    public Service(int id, string name, decimal price)
-    {
-        Id = id;
-        Name = name;
-        Price = price;
-    }
-}
+
 
 public class Stylist
 {
