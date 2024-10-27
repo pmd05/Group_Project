fef1d6a44d10d83f514bf86f66671e60cfd76d26
vovankiet-046205001049 <kietvv1049@ut.edu.vn>
2024-10-27 17:51:03 +0700

    Update AppointmentBookingandManagement.cs

--- BackEnd/AppointmentBookingandManagement.cs
+++ BackEnd/AppointmentBookingandManagement.cs
@@ -29,23 +29,7 @@ public class Service
 }
 
 
-public class Appointment
-{
-    public int Id { get; set; }
-    public Service Service { get; set; }
-    public Stylist Stylist { get; set; }
-    public DateTime Date { get; set; }
-    public bool IsConfirmed { get; set; }
 
-    public Appointment(int id, Service service, Stylist stylist, DateTime date)
-    {
-        Id = id;
-        Service = service;
-        Stylist = stylist;
-        Date = date;
-        IsConfirmed = false;
-    }
-}
 
 using System;
 using System.Collections.Generic;
