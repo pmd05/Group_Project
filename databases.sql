f31ead200bf15ca842a6e352936afbf25561622b
pmd05 <duypm1750@ut.edu.vn>
2024-11-03 13:14:09 +0700

    Update databases.sql

--- databases.sql
+++ databases.sql
@@ -1,1 +1,10 @@
+CREATE TABLE [dbo].[admin] (
+    [id]           INT           IDENTITY (1, 1) NOT NULL,
+    [email]        VARCHAR (MAX) NULL,
+    [username]     VARCHAR (MAX) NULL,
+    [passw]        VARCHAR (MAX) NULL,
+    [date_created] DATE          NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
 
