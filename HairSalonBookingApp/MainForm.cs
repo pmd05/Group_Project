20c8155a977b774d7c439c811f5b8d04306ea773
pmd05 <duypm1750@ut.edu.vn>
2024-11-04 21:38:34 +0700

    Add files via upload

---
+++ HairSalonBookingApp/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HairSalonBookingApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hãy đăng nhập trước");
+        }
+
+        private void Main_Loginbtn_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void Main_Registerbtn_Click(object sender, EventArgs e)
+        {
+            RegisterForm sForm = new RegisterForm();
+            sForm.Show();
+            this.Hide();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HairSalonBookingApp/User.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HairSalonBookingApp
+{
+    public partial class User : Form
+    {
+        bool SidebarExpand;
+        bool homeCollapsed;
+        public User()
+        {
+            InitializeComponent();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void User_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SidebarTimer_Tick(object sender, EventArgs e)
+        {
+            if (SidebarExpand)
+            {
+                sidebar.Width -= 10;
+                if (sidebar.Width == sidebar.MinimumSize.Width)
+                {
+                    SidebarExpand = false;
+                    sidebarTimer.Stop();
+                }
+
+            }
+            else
+            {
+                sidebar.Width += 10;
+                if (sidebar.Width == sidebar.MaximumSize.Width)
+                {
+                    SidebarExpand = true;
+                    sidebarTimer.Stop();
+                }
+            }
+        }
+
+        private void Menu_btn_Click(object sender, EventArgs e)
+        {
+            sidebarTimer.Start();
+        }
+
+        private void HomeContainer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void SettingTimer_Tick(object sender, EventArgs e)
+        {
+            if (homeCollapsed)
+            {
+                HomeContainer.Height += 10;
+                if (HomeContainer.Height == HomeContainer.MaximumSize.Height)
+                {
+                    homeCollapsed = false;
+                    SettingTimer.Stop();
+                }
+            }
+            else
+            {
+                HomeContainer.Height -= 10;
+                if (HomeContainer.Height == HomeContainer.MinimumSize.Height)
+                {
+                    homeCollapsed = true;
+                    SettingTimer.Stop();
+                }
+
+            } 
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            SettingTimer.Start();
+        }
+    }
+}
