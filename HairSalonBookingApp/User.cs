02c9fa0e349988fe050810a7408fb551a06a21a8
HuyNgoVan_0802 <VanHuytqn0802@gmail.com>
2024-11-05 15:28:44 +0700

    Update User.cs

--- HairSalonBookingApp/User.cs
+++ HairSalonBookingApp/User.cs
@@ -15,6 +15,7 @@ namespace HairSalonBookingApp
     {
         bool SidebarExpand;
         bool homeCollapsed;
+        private ErrorProvider errorProvider1 = new ErrorProvider();
         public User()
         {
             InitializeComponent();
@@ -55,20 +56,22 @@ namespace HairSalonBookingApp
             if (SidebarExpand)
             {
                 sidebar.Width -= 10;
-                if (sidebar.Width == sidebar.MinimumSize.Width)
+                if (sidebar.Width <= sidebar.MinimumSize.Width)
                 {
-                    SidebarExpand = false;
+                    sidebar.Width = sidebar.MinimumSize.Width;  
                     sidebarTimer.Stop();
+                    sidebarTimer.Enabled = false; 
                 }
 
             }
             else
             {
                 sidebar.Width += 10;
-                if (sidebar.Width == sidebar.MaximumSize.Width)
+                if (sidebar.Width >= sidebar.MaximumSize.Width)
                 {
-                    SidebarExpand = true;
+                    sidebar.Width = sidebar.MaximumSize.Width;
                     sidebarTimer.Stop();
+                    Sidebar.Expand = false;
                 }
             }
         }
@@ -107,15 +110,101 @@ namespace HairSalonBookingApp
         }
 
         private void button17_Click(object sender, EventArgs e)
-        {
+            {
             SettingTimer.Start();
-        }
+            }
 
         private void button16_Click_1(object sender, EventArgs e)
-        {
+            {
             MainForm mForm = new MainForm();
             mForm.Show();
             this.Hide();
-        }
+            }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+            {
+                string fullName = textBox2.Text.Trim(); 
+                if (string.IsNullOrEmpty(fullName))
+                    {
+                    errorProvider1.SetError(textBox2, "Vui lòng nhập họ tên.");
+                    }
+                else
+                    {
+                        errorProvider1.SetError(textBox2, "");
+                    }
+            }
+        private void textBox2_Leave(object sender, EventArgs e)
+            {
+                textBox2_TextChanged(sender, e);   
+            }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+            {
+                string phone = textBox1.Text;
+                if (!Regex.IsMatch(phone, @"^\d{10}$"))
+                    {
+                        errorProvider1.SetError(textBox1, "Số điện thoại không hợp lệ. Vui lòng nhập 10 số.");
+                    }
+                else
+                    {
+                        errorProvider1.SetError(textBox1, "");
+                    }
+            }
+        private void textBox1_Leave(object sender, EventArgs e)
+            {
+                textBox1_TextChanged(sender, e); 
+            }
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+            {
+
+            }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+            {
+
+            }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+            {
+
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("Cắt tóc: Dịch vụ cắt tóc nam/nữ, tạo kiểu theo yêu cầu. Giá cả tùy thuộc vào độ dài và kiểu tóc.");
+            }
+
+        private void button6_Click(object sender, EventArgs e)
+            {
+
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("Uốn tóc: Dịch vụ uốn tóc xoăn, uốn duỗi, phục hồi hư tổn.  Giá cả tùy thuộc vào độ dài và loại tóc.");
+            }
+
+        private void button4_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("Nhuộm tóc: Dịch vụ nhuộm tóc với nhiều màu sắc đa dạng. Giá cả tùy thuộc vào độ dài và loại thuốc nhuộm.");
+            }
+
+        private void button5_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("Dịch vụ gội đầu thư giãn, sử dụng dầu gội và dầu xả chất lượng cao. Giá cả cố định.");
+            }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+            {
+
+            }
+
+        private void button14_Click(object sender, EventArgs e)
+            {
+
+            }
+
+        private void button12_Click(object sender, EventArgs e)
+            {
+
+            }
     }
 }
